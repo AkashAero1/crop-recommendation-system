42f1fd95c0a445742e152765d14411b7fd55de3e
AkashAero1 <akashrock0950@gmail.com>
2025-09-08 04:25:48 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import time 
 
-API_URL = "http://127.0.0.1:8000/predict" 
+API_URL = "https://crop-recommendation-system-16.onrender.com/predict" 
 
 st.set_page_config(page_title="🌱 Crop Recommendation System", layout="centered")
 
